2bf4d8b90f68f6359812ae468c143c9263aef9da
Toke Voltelen <tokevoltelen@gmail.com>
2026-06-06 12:28:49 +0200

    Keep hash in url when redirecting to latest version

--- src/routes/[package]/+page.ts
+++ src/routes/[package]/+page.ts
@@ -2,9 +2,9 @@ import { redirect } from '@sveltejs/kit';
 import { url } from '$lib/url.js';
 import type { PageLoadEvent } from './$types.js';
 
-export function load({ params }: PageLoadEvent) {
+export function load({ params, url: pageUrl, ...rest }: PageLoadEvent) {
 	const packageVersions = __VERSIONS__[params.package];
 	const target = packageVersions?.length ? url(params.package, packageVersions[0]) : url();
 
-	redirect(307, target);
+	redirect(307, target + pageUrl.hash);
 }
