8f4c66ab6f4e5e5f49a076e4ee3e8c56c8df7ea1
Toke Voltelen <tokevoltelen@gmail.com>
2026-06-06 12:56:54 +0200

    Fix redirecting to hash

---
+++ src/routes/[package]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { browser } from '$app/environment';
+
+let { data } = $props();
+
+if (browser) {
+	window.location.replace(data.target + window.location.hash);
+}
+</script>
--- src/routes/[package]/+page.ts
+++ src/routes/[package]/+page.ts
@@ -1,10 +1,9 @@
-import { redirect } from '@sveltejs/kit';
 import { url } from '$lib/url.js';
 import type { PageLoadEvent } from './$types.js';
 
-export function load({ params, url: pageUrl, ...rest }: PageLoadEvent) {
+export function load({ params }: PageLoadEvent) {
 	const packageVersions = __VERSIONS__[params.package];
 	const target = packageVersions?.length ? url(params.package, packageVersions[0]) : url();
 
-	redirect(307, target + pageUrl.hash);
+	return { target };
 }
